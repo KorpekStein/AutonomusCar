6fe9493ef51f72989ae5df3e3f553cc0fe6d0a0d
Michał <michalek.bryl@gmail.com>
2021-04-23 12:10:28 +0200

    master sketch progress

--- MasterCode.ino
+++ MasterCode.ino
@@ -1,12 +1,27 @@
 #include <Wire.h>
 #include <AFMotor.h>
-
+#include <Servo.h>
 #define pass (void)0
 #define I2C_SLAVE1_ADDRESS 11
 #define PAYLOAD_SIZE 2
+#define MAX_SWERVE 30
+
+Servo servo;
+int angle = 90;
+int angle_change = 5;
 
 
+
+char car_state = 'S';
+char car_mode = 'C'; // C for controlled by user, A for autonomous parking mode
 int n=0;
+
+const int servo_pin = 1;
+const int motor1_pin = 2;
+const int motor2_pin = 3;
+
+AF_DCMotor motor1(motor1_pin);
+AF_DCMotor motor2(motor2_pin);
 
 void setup()
 {
@@ -33,8 +48,50 @@ void setup()
 
 }
 
+void motorsSetup()
+{
+    motor1.setSpeed(255);
+    motor2.setSpeed(255);
+    Stop();
+}
 
 void loop()
 {
+    car_mode=Serial.read();
+    car_state=Serial.read();
+
+    if (car_mode == 'C')
+        controlled_mode();
+    else
+        autonomous_mode();
+
     delay(100);
+}
+
+void controlled_mode()
+{
+    pass;
+}
+void autonomous_mode()
+{
+    pass;
+}
+
+
+//----------MOTION FUNCTIONS
+void go_forward()
+{
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+}
+void turn(int angle)
+{
+    if (angle > MAX_SWERVE)
+        return;
+
+    servo.write(angle);
+}
+void Stop()
+{
+    pass;
 }
