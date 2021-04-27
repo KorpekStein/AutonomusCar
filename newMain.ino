7cb7af405fbd11bd114ae0e19ad13e2c45c40284
Michał <michalek.bryl@gmail.com>
2021-04-27 14:34:28 +0200

    1 file 1 try

--- newMain.ino
+++ newMain.ino
@@ -1,13 +1,22 @@
 #include <Wire.h>
 #include <vector>
+#include <Array.h>
 #define pass (void)0
+#define MAX_ARRAYSIZE 10
 int n = 0;
-
+int speed = 0;
+/* NOTE
+ * THIS CONFIGURATION CONSIST ONLY FROM 2 ONE-SIDE SENSORS
+ *
+ *
+ *
+ */
 //----------Setup----------------------------
 const int Sensor_1_EchoPin = 0;
 const int Sensor_1_TrigPin = 1;
 const int Sensor_2_EchoPin = 2;
 const int Sensor_2_TrigPin = 3;
+/*
 const int Sensor_3_EchoPin = 4;
 const int Sensor_3_TrigPin = 5;
 const int Sensor_4_EchoPin = 6;
@@ -17,19 +26,27 @@ const int Sensor_5_TrigPin = 9;
 const int Sensor_6_EchoPin = 10;
 const int Sensor_6_TrigPin = 11;
 
-/*int Sensor1_distance;
-int Sensor2_distance;
-int Sensor3_distance;
-int Sensor4_distance;
-int Sensor5_distance;
-int Sensor6_distance;*/
+*/
  //TODO FIND HOW TO STORE DATA
-std::vector< float > Sensor1_distances;
-std::vector< float > Sensor2_distances;
-std::vector< float > Sensor3_distances;
-std::vector< float > Sensor4_distances;
-std::vector< float > Sensor5_distances;
-std::vector< float > Sensor6_distances;
+
+//typedef Array<float,MAX_ARRAYSIZE> Sensor1_distances;
+//typedef Array<float,MAX_ARRAYSIZE> Sensor2_distances;
+//typedef Array<float,MAX_ARRAYSIZE> Sensor3_distances;
+//typedef Array<float,MAX_ARRAYSIZE> Sensor4_distances;
+//typedef Array<float,MAX_ARRAYSIZE> Sensor5_distances;
+//typedef Array<float,MAX_ARRAYSIZE> Sensor6_distances;
+
+
+float Sensor1New = 0;
+float Sensor2New = 0;
+float Sensor1Old = 0;
+float Sensor2Old = 0;
+
+AF_DCMotor Right_front_Motor(5);
+AF_DCMotor Left_front_Motor(6);
+AF_DCMotor Right_back_Motor(7);
+AF_DCMotor Left_back_Motor(8);
+
 
 void setup()
 {
@@ -43,7 +60,7 @@ void PinSetup()
 
     pinMode(Sensor_2_TrigPin, OUTPUT);
     pinMode(Sensor_2_EchoPin, INPUT);
-
+/*
     pinMode(Sensor_3_TrigPin, OUTPUT);
     pinMode(Sensor_3_EchoPin, INPUT);
 
@@ -55,17 +72,15 @@ void PinSetup()
 
     pinMode(Sensor_6_TrigPin, OUTPUT);
     pinMode(Sensor_6_EchoPin, INPUT);
-
+*/
 }
 //----------------------------------mainloop--------------------
 void loop()
 {
-    Sensor1_distance.push_back(GetDistance(Sensor_1_TrigPin,Sensor_1_EchoPin));
-    Sensor2_distance.push_back(GetDistance(Sensor_2_TrigPin,Sensor_2_EchoPin));
-    Sensor3_distance.push_back(GetDistance(Sensor_3_TrigPin,Sensor_3_EchoPin));
-    Sensor4_distance.push_back(GetDistance(Sensor_4_TrigPin,Sensor_4_EchoPin));
-    Sensor5_distance.push_back(GetDistance(Sensor_5_TrigPin,Sensor_5_EchoPin));
-    Sensor6_distance.push_back(GetDistance(Sensor_6_TrigPin,Sensor_6_EchoPin));
+    Sensor1Old = Sensor1New;
+    Sensor2Old = Sensor2New;
+    Sensor1New = GetDistance(Sensor_1_TrigPin,Sensor_1_EchoPin);
+    Sensor2New = GetDistance(Sensor_2_TrigPin,Sensor_2_EchoPin);
 }
 
 float GetDistance(int TrigPin, int EchoPin)
@@ -83,25 +98,50 @@ float GetDistance(int TrigPin, int EchoPin)
 
 }
 
-
-//--------------Master-Slave Connection----------------------
-void requestEvents()
+void setspeed(int speed)
 {
-    Serial.println(F("---> recieved request"));
-    Serial.print(F("sending value : "));
-    Serial.println(Sensor1_distance);
-    Wire.write(Sensor1_distance);
+    Right_front_Motor.setSpeed(speed);
+    Left_front_Motor.setSpeed(speed);
+    Right_back_Motor.setSpeed(speed);
+    Left_back_Motor(speed);
 }
 
-void receiveEvents(int numBytes)
+void go_forward()
 {
-    Serial.println(F("---> recieved events"));
-    n = Wire.read();
-    Serial.print(numBytes);
-    Serial.println(F("bytes recieved"));
-    Serial.print(F("recieved value : "));
-    Serial.println(n);
+    Right_front_Motor.run(FORWARD);
+    Left_front_Motor.run(FORWARD);
+    Right_back_Motor.run(FORWARD);
+    Left_back_Motor.run(FORWARD);
 }
+
+void go_backward()
+{
+    Right_front_Motor.run(BACKWARD);
+    Left_front_Motor.run(BACKWARD);
+    Right_back_Motor.run(BACKWARD);
+    Left_back_Motor.run(BACKWARD);
+}
+
+void turn_right()
+{
+    Right_front_Motor.run(BACKWARD);
+    Left_front_Motor.run(FORWARD);
+    Right_back_Motor.run(BACKWARD);
+    Left_back_Motor.run(FORWARD);
+}
+
+void turn_left()
+{
+    Right_front_Motor.run(FORWARD);
+    Left_front_Motor.run(BACKWARD);
+    Right_back_Motor.run(FORWARD);
+    Left_back_Motor.run(BACKWARD);
+}
+
+
+
+
+
 //-------------------------------looking for space to park-------------------------------
 
 int look_for_parallel()
